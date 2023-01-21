48402da9c50248885ed77a73a93c7900a289c8f8
Rhonda Guillory <guilloryrt.ps@gmail.com>
2023-01-21 15:32:45 -0600

    classes-exercise commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("");
+            Car car = new Car() ;
+            car.Make = "Toyota";
+            car.Model = "Camry";
+            car.Year = 2022;
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+
         }
     }
 }
